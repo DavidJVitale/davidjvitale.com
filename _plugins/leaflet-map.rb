c9f75c3819f399e13abb70709fbaffd1cafcae7b
DavidJVitale <18691334+DavidJVitale@users.noreply.github.com>
2019-06-03 21:13:12 -0700

    2 maps drawing, script is an anonymous function

--- _plugins/leaflet-map.rb
+++ _plugins/leaflet-map.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 module Jekyll
   class RenderTimeTagBlock < Liquid::Block
 
@@ -11,7 +12,8 @@ module Jekyll
         leaflet_providers_js_content = File.read("./_plugins/leaflet-providers.js")
 
         map_preparsed_html = File.read("./_plugins/leaflet-map.html")
-        map_parsed_html = map_preparsed_html % {leaflet_providers_js_content: leaflet_providers_js_content,
+        map_parsed_html = map_preparsed_html % {id: SecureRandom.hex,
+                                                leaflet_providers_js_content: leaflet_providers_js_content,
                                                 tag_input_arg_json: @input,
                                                 inside_block_leaflet_objects: text}
         map_parsed_html
